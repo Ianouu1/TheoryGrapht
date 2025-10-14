a150964f6ccee724fe2112c3944746aada6d6f47
courrierdsf <courrierdsf@gmail.com>
2025-10-14 16:46:03 +0200

    BFS now return edge relations

--- src/main/java/org/theorygrapht/service/BFS.java
+++ src/main/java/org/theorygrapht/service/BFS.java
@@ -1,5 +1,6 @@
 package org.theorygrapht.service;
 
+import org.theorygrapht.model.Edge;
 import org.theorygrapht.model.Graph;
 import org.theorygrapht.model.Vertex;
 import org.theorygrapht.util.DummyGraph;
@@ -9,23 +10,21 @@ import java.util.*;
 import static org.theorygrapht.util.GraphUtils.searchVertex;
 
 public class BFS {
-    public static List<Vertex> getBFS(Graph graph, String startingVertexName) {
+    public static List<Edge> getBFS(Graph graph, String startingVertexName) {
         Vertex[] vertices = graph.getVertices();
-
         Vertex start = searchVertex(vertices, startingVertexName);
 
         Set<Vertex> visited = new HashSet<>();
         Queue<Vertex> queue = new LinkedList<>();
-        List<Vertex> visitingOrder = new ArrayList<>();
+        List<Edge> traversalEdges = new ArrayList<>();
 
         visited.add(start);
         queue.add(start);
 
         while (!queue.isEmpty()) {
             Vertex current = queue.poll();
-            visitingOrder.add(current);
 
-            for (var edge : graph.getAdjacencyList().get(current)) {
+            for (Edge edge : graph.getAdjacencyList().get(current)) {
                 Vertex neighbor = edge.getSource().equals(current)
                         ? edge.getTarget()
                         : edge.getSource();
@@ -33,9 +32,11 @@ public class BFS {
                 if (!visited.contains(neighbor)) {
                     visited.add(neighbor);
                     queue.add(neighbor);
+                    traversalEdges.add(edge);
                 }
             }
         }
-        return visitingOrder;
+
+        return traversalEdges;
     }
 }
