7f0edaebc4239e8cc440849d2bfcac2f7ef5b51c
courrierdsf <courrierdsf@gmail.com>
2025-10-16 01:41:14 +0200

    (IGNORE) Sandbox files

--- src/main/java/org/theorygrapht/MainFlorian.java
+++ src/main/java/org/theorygrapht/MainFlorian.java
@@ -3,46 +3,64 @@ package org.theorygrapht;
 import org.theorygrapht.model.Edge;
 import org.theorygrapht.model.Graph;
 import org.theorygrapht.model.Vertex;
-import org.theorygrapht.service.Prim;
 import org.theorygrapht.util.DummyGraph;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.theorygrapht.service.BFS.getBFS;
+import static org.theorygrapht.service.DFS.getDFS;
 import static org.theorygrapht.service.Kruskal.getKruskal;
 import static org.theorygrapht.service.Prim.getPrim;
 import static org.theorygrapht.service.Dijkstra.getDijkstra;
-import static org.theorygrapht.service.DFS.getDFS;
+import static org.theorygrapht.service.Dijkstra.getDijkstraFinalMatrix;
+import static org.theorygrapht.service.BellmanFord.getBellmanFord;
+import static org.theorygrapht.service.FloydWarshall.getFloydWarshall;
+
 
 
 public class MainFlorian {
     public static void main(String[] args) {
+        Graph graph = DummyGraph.create();
+
         System.out.println("Algo de Kruskal :");
-        Map<String, Object> Kruskal = getKruskal();
+        List<Edge> Kruskal = getKruskal(graph);
         System.out.println(Kruskal);
         System.out.println("-----\n");
 
         System.out.println("Algo de Prim :");
-        Map<String, Object> prim = getPrim("Paris");
+        List<Edge> prim = getPrim(graph, "Paris");
         System.out.println(prim);
         System.out.println("-----\n");
 
         System.out.println("Algo de Dijkstra :");
-        List<Edge> Djikstra = getDijkstra("Bordeaux", "Lille");
+        Map<String, Integer> Dijkstra2 = getDijkstraFinalMatrix(graph, "Bordeaux");
+        System.out.println(Dijkstra2);
+        System.out.println("-----\n");
+
+        System.out.println("Algo de Dijkstra Plus Court Chemin:");
+        List<Edge> Djikstra = getDijkstra(graph, "Bordeaux", "Lille");
         System.out.println(Djikstra);
         System.out.println("-----\n");
 
-
         System.out.println("Algo de BFS (largeur): <!> On traite ici directement les données sous forme de sommets");
-        List<Vertex> BFS = getBFS("Rennes");
+        List<Edge> BFS = getBFS(graph, "Rennes");
         System.out.println(BFS);
         System.out.println("-----\n");
 
         System.out.println("Algo de DFS (profondeur) : <!> On traite ici directement les données sous forme de sommets");
-        List<Vertex> DFS = getDFS("Rennes");
+        List<Edge> DFS = getDFS(graph, "Rennes");
         System.out.println(DFS);
         System.out.println("-----\n");
 
+        System.out.println("Algo de Bellman-Ford :");
+        Map<Vertex, Integer> BellmanFord = getBellmanFord(graph,"Bordeaux");
+        System.out.println(BellmanFord);
+        System.out.println("-----\n");
+
+        System.out.println("Algo de Floyd-Warshall :");
+        List<Edge> FloydWarshall = getFloydWarshall(graph, "Grenoble", "Rennes");
+        System.out.println(FloydWarshall);
+        System.out.println("-----\n");
     }
 }
--- src/main/java/org/theorygrapht/MainYannis.java
+++ src/main/java/org/theorygrapht/MainYannis.java
@@ -1,13 +1,13 @@
 package org.theorygrapht;
 
-import org.theorygrapht.model.Edge;
 import org.theorygrapht.model.Graph;
-import org.theorygrapht.service.Kruskal;
-import org.theorygrapht.service.Dijkstra;
 import org.theorygrapht.util.DummyGraph;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import static org.theorygrapht.service.Dijkstra.getDijkstraFinalMatrix;
+import static org.theorygrapht.service.Dijkstra.getDijkstraTable;
 
 public class MainYannis {
 
@@ -16,25 +16,14 @@ public class MainYannis {
         Graph graph = DummyGraph.create();
         System.out.println("Graphe créé avec succès !");
 
-        // Exécuter Kruskal
-//        List<Edge> mst = Kruskal.getKruskal();
-//        System.out.println(mst);
-//        System.out.println("Résultat de l'algorithme de Kruskal :");
-//        Arrays.stream(mst).forEach(System.out::println);
-//
-//        int totalDistance = Arrays.stream(mst)
-//                .mapToInt(s -> {
-//                    String[] parts = s.split(": ");
-//                    return Integer.parseInt(parts[1]);
-//                })
-//                .sum();
-//        System.out.println("Distance totale Kruskal : " + totalDistance);
+        System.out.println("Algo de Dijkstra :");
+        List<Map<String, String>> Dijkstra2 = getDijkstraTable(graph, "Bordeaux");
+        for (Map<String, String> map : Dijkstra2) {
+            System.out.println(map);
+        }
+        System.out.println("-----\n");
 
-        // --------------------------
-        // Exécuter Dijkstra
-        // --------------------------
-//        String[] distances = Dijkstra.getDijkstra();
-//        System.out.println("\nRésultat de l'algorithme de Dijkstra (depuis le premier sommet) :");
-//        Arrays.stream(distances).forEach(System.out::println);
+        System.out.println(getDijkstraFinalMatrix(graph, "Bordeaux"));
+
     }
 }
